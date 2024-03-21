16b32144939bddd3027dba846df0c57d60a62943
Carlos Oliveira <carlos.oli1505@gmail.com>
2024-03-21 20:48:15 -0300

    login endpoint on authentication controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,40 @@
 import { createUser, getUserByEmail } from '../db/users';
-import express from 'express';
+import express, { NextFunction } from 'express';
 import { authentication, random } from '../helpers';
+
+export const login = async (req: express.Request, res: express.Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password !== expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessiontToken = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('CARLOS-AUTH', user.authentication.sessiontToken, { domain: 'localhost', path: '/' });
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
 
 export const register = async (req: express.Request, res: express.Response) => {
     try {
